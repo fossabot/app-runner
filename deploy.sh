35344507557b4cfea4136bd4df11209e43740e36
Bruno Santos <bruno.santos@zoop.com.br>
2021-05-26 07:20:59 -0300

    Inclusao de variaveis para indicar regiao e conta da aws

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 882456490456.dkr.ecr.us-east-1.amazonaws.com
-docker build -t myrepo /home/bruno.santos/codes/poc/app-runner/. 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $1.dkr.ecr.us-east-1.amazonaws.com
+#docker build -t myrepo /home/bruno.santos/codes/poc/app-runner/. 
+docker build -t myrepo . 
 
-if [ $# -eq 0 ]; then
-  docker tag myrepo:latest 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest 
-  docker push 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest
-else
-  docker tag myrepo:latest 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:$1 
-  docker push 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:$1
-fi
+#if [ $# -eq 0 ]; then
+docker tag myrepo:latest $1.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest 
+docker push $1.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest
+#else
+  #docker tag myrepo:latest 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:$1 
+  #docker push 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:$1
+#fi
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_ecr_repository" "myrepo" {
@@ -11,7 +11,7 @@ resource "aws_ecr_repository" "myrepo" {
 
   provisioner "local-exec" {
     when    = destroy
-    command = "sh ~/codes/poc/app-runner/removeimages.sh"
+    command = "sh ../removeimages.sh"
   }
 
 }
@@ -19,76 +19,54 @@ resource "aws_ecr_repository" "myrepo" {
 resource "null_resource" "myimage" {
 
   provisioner "local-exec" {
-    command = "sh ~/codes/poc/app-runner/deploy.sh"
-    #aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 882456490456.dkr.ecr.us-east-1.amazonaws.com && docker build -t myrepo /home/bruno.santos/codes/poc/app-runner/. && docker tag myrepo:latest 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest && docker push 882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest"
+    command = "sh ../deploy.sh $aws_account"
   }
 }
 
 resource "aws_apprunner_service" "apprunner" {
-    service_name                   = "apprunner"
+  service_name = "apprunner"
 
-    health_check_configuration {
-        healthy_threshold   = 1
-        interval            = 10
-        path                = "/"
-        protocol            = "TCP"
-        timeout             = 5
-        unhealthy_threshold = 5
-    }
-
-    source_configuration {
-        auto_deployments_enabled = false
-
-        authentication_configuration {
-            access_role_arn = "arn:aws:iam::882456490456:role/service-role/AppRunnerECRAccessRole"
-        }
-
-        image_repository {
-            image_identifier      = "882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest"
-            image_repository_type = "ECR"
-
-            image_configuration {
-                port                          = "8000"
-                runtime_environment_variables = {}
-            }
-        }
-    }
-}
-
-/*
-resource "aws_apprunner_service" "myapp" {
-  service_name = "myapp"
-
-  source_configuration {
-    image_repository {
-      image_configuration {
-        port = "8000"
-      }
-      image_identifier      = "882456490456.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest"
-      image_repository_type = "ECR"
-    }
+  health_check_configuration {
+    healthy_threshold   = 1
+    interval            = 10
+    path                = "/"
+    protocol            = "TCP"
+    timeout             = 5
+    unhealthy_threshold = 5
   }
 
-  depends_on = [
-    null_resource.myimage,
-  ]
-}
-*/
+  source_configuration {
 
-/*
+    auto_deployments_enabled = false
+
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::${var.aws_account}:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    image_repository {
+      image_identifier      = "${var.aws_account}.dkr.ecr.us-east-1.amazonaws.com/myrepo:latest"
+      image_repository_type = "ECR"
+
+      image_configuration {
+        port                          = "8000"
+        runtime_environment_variables = {}
+      }
+    }
+  }
+}
+
 output "ecr_arn" {
-  value = aws_ecrpublic_repository.myrepo.arn
-}
-
-output "ecr_repository" {
-  value = aws_ecrpublic_repository.myrepo.repository_uri
+  value = aws_ecr_repository.myrepo.arn
 }
 
 output "ecr_registry" {
-  value = aws_ecrpublic_repository.myrepo.registry_id
+  value = aws_ecr_repository.myrepo.registry_id
 }
 
-output "ecr_id" {
-  value = aws_ecrpublic_repository.myrepo.id
+output "ecr_repository" {
+  value = aws_ecr_repository.myrepo.repository_url
 }
-*/
+
+output "name" {
+  value = aws_apprunner_service.apprunner.service_url
+}
